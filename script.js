b88b7671068841906a23cab50d546a4ce5e14f29
gaby magnussen <gabymagnussen@gmail.com>
2020-02-02 10:40:37 -0600

    updated index, script, and style

--- script.js
+++ script.js
@@ -1,11 +1,32 @@
 var apiKey = '9016d79680b228468f8bc62eaad01307';
 var baseUrl = 'https://api.openweathermap.org/data/2.5/weather?q=';
-var locationQuery = 'Dallas,US';
-$(document).ready(function () {
+// var queryUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + apiKey + searchCity;
+
+var searchCity = $("#searchCity").val();
+
+// ----------------------------
+
+//querying the openweathermap API for weather data in the city that is being searched
+function searchWeather(searchCity) {
+    var queryUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + searchCity + "&APPID=" + apiKey;
     $.ajax({
-        url: baseUrl + locationQuery + '&appId=' + apiKey,
-        method: 'GET'
-    }).then(function (results) {
-        console.log(results);
+      url: queryUrl,
+      method: "GET"
+    }).then(function(response) {
+      console.log(response);
+    
+
+
+    
     });
-});
+  }
+
+
+  //event handler for search for city button
+  $("#queryBtn").on("click", function(event) {
+    event.preventDefault();
+   var inputCity = $("#searchCity").val().trim();
+    searchWeather(inputCity);
+  });
+
+
