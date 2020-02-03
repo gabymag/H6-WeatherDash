7d72aabb51226d5f20b11ea16cef57643cd84fc9
gaby magnussen <gabymagnussen@gmail.com>
2020-02-02 18:27:04 -0600

    started 5 day forecast

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ var searchCity = $("#searchCity").val();
 
 //querying the openweathermap API for weather data in the city that is being searched
 function searchWeather(searchCity) {
-    var queryUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + searchCity + "&APPID=" + apiKey;
+    var queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + searchCity + "&APPID=" + apiKey;
 
     $.ajax({
       url: queryUrl,
@@ -32,29 +32,50 @@ function searchWeather(searchCity) {
     });
   }
 
-  //event handler for search for city button
-  $("#queryBtn").on("click", function(event) {
-    event.preventDefault();
-   var inputCity = $("#searchCity").val().trim();
-    searchWeather(inputCity);
-  });
+ 
 
 
   function getForecast(searchCity) {
     //get 5 day forecast
+    // var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" 
+    // + searchCity + "&APPID=" + apiKey;
+    // var queryURL = "https://api.openweathermap.org/data/2.5/forecast/daily?id=" + searchCity + "&cnt=5 " + apiKey;
+
     var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + searchCity + "&APPID=" + apiKey;
+
+    // var queryURL= "https://api.openweathermap.org/data/2.5/forecast?id=" + Searchcity + "&APPID=9016d79680b228468f8bc62eaad01307&units=5";
+
     $.ajax({
-        
         url: queryURL,
         method: "GET"
     }).then(function (response) {
         console.log('5response :', response);
+        // var WeatherImg = "https://openweathermap.org/img/wn/" + response.weather[0].icon + "@2x.png";
+        $("#tempOne").html("<h3>" + 
+        (Math.round ((response.list[0].main.temp -273.15) * 1.80 +32 )+ " &degF</h3>"));
+        $("#tempTwo").html("<h3>" + 
+        (Math.round ((response.list[1].main.temp -273.15) * 1.80 +32 )+ " &degF</h3>"));
+
+
+
+        $("#humiOne").html("<p>Humidity: " + response.list[0].main.humidity + "%</p>");
+        $("#humiTwo").html("<p>Humidity: " + response.list[2].main.humidity + "%</p>");
+
+
       
 
 
         }
     )};
 
+ //event handler for search for city button
+ $("#queryBtn").on("click", function(event) {
+    event.preventDefault();
+   var inputCity = $("#searchCity").val().trim();
+    searchWeather(inputCity);
+    getForecast(inputCity);
+
+  });
 
 
  
