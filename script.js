ba2fe3d5351a7d3c8e45eed1e5d7414b7629b107
gaby magnussen <gabymagnussen@gmail.com>
2020-02-03 11:47:25 -0600

    added dates to five day forecast

--- script.js
+++ script.js
@@ -7,8 +7,6 @@ var fiveDayUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=';
 
 var searchCity = $("#searchCity").val();
 
-// ----------------------------
-
 //querying the openweathermap API for weather data in the city that is being searched
 function searchWeather(searchCity) {
     var queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + searchCity + "&APPID=" + apiKey;
@@ -20,7 +18,7 @@ function searchWeather(searchCity) {
       console.log(response);
 
       var currentWeatherImg = "https://openweathermap.org/img/wn/" + response.weather[0].icon + "@2x.png";
-
+//taking the response and inputting it into the HTML elements
         $("#city").html("<h1>" + response.name + " Weather Today</h1>");
         // $("#WeatherIcon").html(+ response.weather[0].icon);
         $("#WeatherIcon").attr("src", currentWeatherImg);
@@ -32,31 +30,34 @@ function searchWeather(searchCity) {
     });
   }
 
- 
-
-
   function getForecast(searchCity) {
     //get 5 day forecast
-    // var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" 
-    // + searchCity + "&APPID=" + apiKey;
-    // var queryURL = "https://api.openweathermap.org/data/2.5/forecast/daily?id=" + searchCity + "&cnt=5 " + apiKey;
 
     var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + searchCity + "&APPID=" + apiKey;
-
-    // var queryURL= "https://api.openweathermap.org/data/2.5/forecast?id=" + Searchcity + "&APPID=9016d79680b228468f8bc62eaad01307&units=5";
 
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function (response) {
         console.log('5response :', response);
-        var WeatherImg = "https://openweathermap.org/img/wn/" + response.list[0].weather[0].icon + "@2x.png";
-        var WeatherImgTwo = "https://openweathermap.org/img/wn/" + response.list[1].weather[0].icon + "@2x.png";
-        var WeatherImgThree = "https://openweathermap.org/img/wn/" + response.list[2].weather[0].icon + "@2x.png";
-        var WeatherImgFour = "https://openweathermap.org/img/wn/" + response.list[3].weather[0].icon + "@2x.png";
-        var WeatherImgFive = "https://openweathermap.org/img/wn/" + response.list[4].weather[0].icon + "@2x.png";
+        // Dates for 5 day forecast cards
+        $("#DayOne").html(moment().add(1,'day').format( "MMMM Do"));
+        $("#DayTwo").html(moment().add(2,'day').format( "MMMM Do"));
+        $("#DayThree").html(moment().add(3,'day').format( "MMMM Do"));
+        $("#DayFour").html(moment().add(4,'day').format( "MMMM Do"));
+        $("#DayFive").html(moment().add(5,'day').format( "MMMM Do"));
+        
 
-        //FIVE DAY FORECAST WEATHER ICON
+
+
+        //making variables for the weather icons
+        var WeatherImg = "https://openweathermap.org/img/wn/" + response.list[8].weather[0].icon + "@2x.png";
+        var WeatherImgTwo = "https://openweathermap.org/img/wn/" + response.list[16].weather[0].icon + "@2x.png";
+        var WeatherImgThree = "https://openweathermap.org/img/wn/" + response.list[24].weather[0].icon + "@2x.png";
+        var WeatherImgFour = "https://openweathermap.org/img/wn/" + response.list[32].weather[0].icon + "@2x.png";
+        var WeatherImgFive = "https://openweathermap.org/img/wn/" + response.list[39].weather[0].icon + "@2x.png";
+
+        //FIVE DAY FORECAST WEATHER ICON INTO THE HTML
         $("#WeatherIconOne").attr("src", WeatherImg);
         $("#WeatherIconTwo").attr("src", WeatherImgTwo);
         $("#WeatherIconThree").attr("src", WeatherImgThree);
@@ -66,32 +67,30 @@ function searchWeather(searchCity) {
 
         ///FIVE DAY FORECAST TEMPERATURES
         $("#tempOne").html("<h3>" + 
-        (Math.round ((response.list[0].main.temp -273.15) * 1.80 +32 )+ " &degF</h3>"));
+        (Math.round ((response.list[8].main.temp -273.15) * 1.80 +32 )+ " &degF</h3>"));
         $("#tempTwo").html("<h3>" + 
-        (Math.round ((response.list[1].main.temp -273.15) * 1.80 +32 )+ " &degF</h3>"));
+        (Math.round ((response.list[16].main.temp -273.15) * 1.80 +32 )+ " &degF</h3>"));
         $("#tempThree").html("<h3>" + 
-        (Math.round ((response.list[2].main.temp -273.15) * 1.80 +32 )+ " &degF</h3>"));
+        (Math.round ((response.list[24].main.temp -273.15) * 1.80 +32 )+ " &degF</h3>"));
         $("#tempFour").html("<h3>" + 
-        (Math.round ((response.list[3].main.temp -273.15) * 1.80 +32 )+ " &degF</h3>"));
+        (Math.round ((response.list[32].main.temp -273.15) * 1.80 +32 )+ " &degF</h3>"));
         $("#tempFive").html("<h3>" + 
-        (Math.round ((response.list[4].main.temp -273.15) * 1.80 +32 )+ " &degF</h3>"));
+        (Math.round ((response.list[39].main.temp -273.15) * 1.80 +32 )+ " &degF</h3>"));
 
 
         //FIVE DAY FORECAST HUMIDITY
-        $("#humiOne").html("<p>Humidity: " + response.list[0].main.humidity + "%</p>");
-        $("#humiTwo").html("<p>Humidity: " + response.list[1].main.humidity + "%</p>");
-        $("#humiThree").html("<p>Humidity: " + response.list[2].main.humidity + "%</p>");
-        $("#humiFour").html("<p>Humidity: " + response.list[3].main.humidity + "%</p>");
-        $("#humiFive").html("<p>Humidity: " + response.list[4].main.humidity + "%</p>");
-
-
-      
-
-
+        $("#humiOne").html("<p>Humidity: " + response.list[8].main.humidity + "%</p>");
+        $("#humiTwo").html("<p>Humidity: " + response.list[16].main.humidity + "%</p>");
+        $("#humiThree").html("<p>Humidity: " + response.list[24].main.humidity + "%</p>");
+        $("#humiFour").html("<p>Humidity: " + response.list[32].main.humidity + "%</p>");
+        $("#humiFive").html("<p>Humidity: " + response.list[39].main.humidity + "%</p>");
         }
     )};
 
- //event handler for search for city button
+
+
+
+ //event handler for search for city button for both the one day and 5 day forecast
  $("#queryBtn").on("click", function(event) {
     event.preventDefault();
    var inputCity = $("#searchCity").val().trim();
